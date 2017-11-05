c21d4c227056308e94720aaa0fbc223f4c0a5e34
orest133 <orest133@gmail.com>
2017-11-05 11:29:53 +0100

    First

---
+++ PracaInzynierska/WorldMapCoordinates.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+//GDAL
+
+#include <gdal/gdal.h>
+#include <gdal/gdal_priv.h>
+#include <gdal/ogrsf_frmts.h>
+
+class WorldMapCoordinates {//класа карти в форматі тіфф , включає в себе геодані  з карти і функції доступу до них 
+private:	int widthMap, heightMap;
+			float topLeftX, topLeftY, topRightX, bottomLeftY;
+			float widthPixel, heightPixel;
+			double adfGeoTransform[6];
+			GDALDataset *poDataset;
+			float coordinateX;
+			float coordinateY;
+			
+public:
+	WorldMapCoordinates() {
+	}
+	WorldMapCoordinates(char patch[]) {
+		GDALAllRegister();
+		poDataset = (GDALDataset*)GDALOpen(patch, GA_ReadOnly);
+
+		if (poDataset->GetGeoTransform(adfGeoTransform) == CE_None)
+		{
+			topLeftX = adfGeoTransform[0];
+			topLeftY = adfGeoTransform[3];
+			heightPixel = adfGeoTransform[5];
+			widthPixel = adfGeoTransform[1];
+
+			widthMap = GDALGetRasterXSize(poDataset);
+			heightMap = GDALGetRasterYSize(poDataset);
+			topRightX = topLeftX + adfGeoTransform[1] * widthMap;
+			bottomLeftY = topLeftY + adfGeoTransform[5] * heightMap;
+			convertGlobalPosToLocal();
+			std::cout << " Coordinate LeftX " << topLeftX << " , " << topLeftY << " RrghtTopX " << topRightX << std::endl << " Lenght pixel " << widthPixel << "," << heightPixel << std::endl << "Data size: " << widthMap << " " << heightMap << std::endl;
+		}
+	}
+	int getWidthMap()
+	{
+		return widthMap;
+	}
+	int getHeightMap()
+	{
+		return heightMap;
+	}
+   void convertGlobalPosToLocal() {
+	   coordinateX = (topLeftX - 692332) / (topRightX - topLeftX);
+	   coordinateY = (210567 - topLeftY) / (bottomLeftY - topLeftY);
+		
+	}
+
+   float getCoordinateX()
+   {
+	   return coordinateX;
+   }
+   float getCoordinateY()
+   {
+	   return coordinateY;
+   }
+   float getTopLeftX()
+   {
+	   return topLeftX;
+   }
+   float getBottomLeftY()
+   {
+	   return bottomLeftY;
+   }
+   float getTopRightX()
+   {
+	   return topRightX;
+   }
+   float getTopLeftY()
+   {
+	   return topLeftY;
+   }
+   float getWidthPixel()
+   {
+	   return widthPixel;
+   }
+};
+
---
+++ PracaInzynierska/main.cpp
@@ -0,0 +1,363 @@
+﻿// Std. Includes
+#include <string>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+// GL includes
+#include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
+// GLM Mathemtics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+// Other Libs
+#include "SOIL2/SOIL2.h"
+#include "Texture.h"
+#include "WorldMapCoordinates.h"
+
+float static central_poxition_x = 689032;//Позиція вписана оператором ,точка відліку 
+float static central_position_y = 212567;//Позиція вписана оператором ,точка відліку 
+
+float pos_object_x = 691364;//позиція дрона 
+float pos_object_y = 210332;//pozycja drona 
+
+void  drowMap(float x, float  y, glm::mat4 model, GLint modelLoc, int index) {
+	glBindTexture(GL_TEXTURE_2D, index);
+	model = glm::translate(model, glm::vec3(x, y, 0));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+// Properties
+const GLuint WIDTH = 1200, HEIGHT = 800;
+int SCREEN_WIDTH, SCREEN_HEIGHT;
+// Function prototypes
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode);
+void MouseCallback(GLFWwindow *window, double xPos, double yPos);
+void DoMovement();
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+int main()
+{
+	vector<WorldMapCoordinates> worldMapCoordinate;
+	worldMapCoordinate.push_back(WorldMapCoordinates("maps/M-34-080-C-d-1-1.tif"));
+	worldMapCoordinate.push_back(WorldMapCoordinates("maps/M-34-080-C-d-1-2.tif"));
+	worldMapCoordinate.push_back(WorldMapCoordinates("maps/M-34-080-C-d-1-3.tif"));
+	worldMapCoordinate.push_back(WorldMapCoordinates("maps/M-34-080-C-d-1-4.tif"));
+	// Init GLFWs
+	glfwInit();
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (nullptr == window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
+	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
+	// Set the required callback functions
+	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
+	// GLFW Options
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Define the viewport dimensions
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	// OpenGL options
+	glEnable(GL_DEPTH_TEST);
+
+	// Setup and compile our shaders
+	Shader shader("res/shaders/cube.vs", "res/shaders/cube.frag");
+	Shader skyboxShader("res/shaders/skybox.vs", "res/shaders/skybox.frag");
+
+
+	GLfloat vertices[] = {
+		// Positions          // Colors           // Texture Coords
+		0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // x=0,y=0
+		0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, //x=0,y=-1
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, //x=-1,y=-1
+		0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // x=0,y=0
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, //x=-1,y=-1
+		-1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //x=0,y=-1
+	};
+
+	GLfloat skyboxVertices[] = {
+		// Positions
+		-1.0f,  1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f
+	};
+
+
+	// Setup cube VAO
+	GLuint cubeVAO, cubeVBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glBindVertexArray(cubeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(6 * sizeof(GLfloat)));
+
+	glBindVertexArray(0);
+
+	// Setup skybox VAO
+	GLuint skyboxVAO, skyboxVBO;
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
+	glBindVertexArray(skyboxVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+	glBindVertexArray(0);
+
+	// Load textures
+	GLuint cubeTexture = TextureLoading::LoadTexture("res/images/container2.png");
+
+	vector<GLuint> textureMap;
+	textureMap.push_back(TextureLoading::LoadTexture("\maps/M-34-080-C-d-1-1.png"));
+	textureMap.push_back(TextureLoading::LoadTexture("\maps/M-34-080-C-d-1-2.png"));
+	textureMap.push_back(TextureLoading::LoadTexture("\maps/M-34-080-C-d-1-3.png"));
+	textureMap.push_back(TextureLoading::LoadTexture("\maps/M-34-080-C-d-1-4.png"));
+
+
+	// Cubemap (Skybox)
+	vector<const GLchar*> faces;
+	faces.push_back("res/images/skyright.png");
+	faces.push_back("res/images/skyleft.png");
+	faces.push_back("res/images/skyup.png");
+	faces.push_back("res/images/skydown.png");
+	faces.push_back("res/images/skyfront.png");
+	faces.push_back("res/images/skyright2.png");
+
+	GLuint cubemapTexture = TextureLoading::LoadCubemap(faces);
+
+	GLuint airplaneTexture = TextureLoading::LoadTexture("res/models/texture.png");
+
+	glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
+	// Load models
+	Model ourModel("res/models/airplane.obj");
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Set frame time
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		// Check and call events
+		glfwPollEvents();
+		DoMovement();
+		// Clear the colorbuffer
+		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glm::mat4 model;
+		glm::mat4 view = camera.GetViewMatrix();
+		// Draw our first triangle
+		shader.Use();
+		// Bind Textures using texture units
+		glActiveTexture(GL_TEXTURE0);
+		// Get the uniform locations
+		GLint modelLoc = glGetUniformLocation(shader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(shader.Program, "view");
+		GLint projLoc = glGetUniformLocation(shader.Program, "projection");
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		//Початок малювання карт
+		glBindVertexArray(cubeVAO);
+		model = glm::rotate(model, 1.59f, glm::vec3(1.0f, 0.0f, 0.0f));
+		int i = 0;
+		for (vector<WorldMapCoordinates>::iterator map = worldMapCoordinate.begin(); map != worldMapCoordinate.end(); ++map, i++)
+			drowMap(map->getCoordinateX(), map->getCoordinateY(), model, modelLoc, textureMap[i]);
+		glBindVertexArray(0);
+		//кінець малювання карт
+
+		//початок малювання літачка 
+		float object_x = (pos_object_x - 692333) / (worldMapCoordinate[1].getTopRightX() - worldMapCoordinate[1].getTopLeftX());
+		float object_y = (210568 - pos_object_y) / (worldMapCoordinate[1].getBottomLeftY() - worldMapCoordinate[1].getTopLeftY());
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		glm::mat4  model2 = model;
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		model2 = glm::translate(model2, glm::vec3(object_x, object_y, 0));
+		model2 = glm::scale(model2, glm::vec3(0.001f, 0.001f, 0.001f));
+		model2 = glm::rotate(model2, 1.59f, glm::vec3(-1.0f, 0.0f, 0.0f));
+		model2 = glm::rotate(model2, 1.59f, glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		ourModel.Draw(shader);
+		//кінець малювання літака 
+
+		// початок малювання оточення 
+		glDepthFunc(GL_LEQUAL);  // Change depth function so depth test passes when values are equal to depth buffer's content
+		skyboxShader.Use();
+		view = glm::mat4(glm::mat3(camera.GetViewMatrix()));	// Remove any translation component of the view matri
+		glUniformMatrix4fv(glGetUniformLocation(skyboxShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(skyboxShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+		glBindVertexArray(skyboxVAO);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+		glDepthFunc(GL_LESS); // Set depth function back to default
+	   // кінець малювання оточення 
+
+		// Swap the buffers
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+
+// Moves/alters the camera positions based on user input
+void DoMovement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
+	{
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
+	{
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
+	{
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
+	{
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (GLFW_KEY_ESCAPE == key && GLFW_PRESS == action)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	if (GLFW_KEY_1 == key)
+	{
+		pos_object_x += 20;
+	}
+	if (GLFW_KEY_2 == key)
+	{
+		pos_object_x -= 20;
+	}
+	if (GLFW_KEY_3 == key)
+	{
+		pos_object_y += 20;
+	}
+	if (GLFW_KEY_4 == key)
+	{
+		pos_object_y -= 20;
+	}
+	if (GLFW_KEY_5 == key)
+		std::cout << pos_object_x << "----" << pos_object_y << endl;
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			keys[key] = false;
+		}
+	}
+}
+
+void MouseCallback(GLFWwindow *window, double xPos, double yPos)
+{
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+
+	GLfloat xOffset = xPos -	lastX;
+	GLfloat yOffset = lastY - yPos;  // Reversed since y-coordinates go from bottom to left
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.ProcessMouseMovement(xOffset, yOffset);
+}
+
