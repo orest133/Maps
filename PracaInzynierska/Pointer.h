a8b0e0cf32bc0e6f3cd989a8f63b217809aa6e4f
orest133 <orest133@gmail.com>
2017-12-17 14:03:24 +0100

    Refactoring

--- PracaInzynierska/Pointer.h
+++ PracaInzynierska/Pointer.h
@@ -2,7 +2,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-//#include <GL/glew.h>
+
 #include "InputControl.h"
 #include "PointsRepository.h"
 
